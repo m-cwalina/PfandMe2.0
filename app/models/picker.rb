b8df3aa8235f35c9404f6692a72f618fb2cb12f0
Matthew Cwalina <matthewcwalina@gmail.com>
2022-07-04 14:14:43 +0200

    Created a user and picker model with devise

---
+++ app/models/picker.rb
@@ -0,0 +1,6 @@
+class Picker < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
