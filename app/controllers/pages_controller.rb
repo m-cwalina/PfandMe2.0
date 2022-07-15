11b31cd2b6ebe6be8b421e943ee4bc1b1a10ebf2
Matthew Cwalina <matthewcwalina@gmail.com>
2022-07-15 17:38:33 +0200

    Cleaned up the dashboard with activerecord queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, except: :home
 
-  def Home
+  def home
   end
 
-  def Dashboard
+  def dashboard
+    # Assigning current_user to instance variable @user
+    @user = current_user
+    # Finding the current user with appoinments in the range of the last 30 days
+    @past_orders = User.find_by(id: current_user).appointments.where(date: Date.today - 30.day...Date.today)
+    # Finding the current users with appointments bottles and summing them up
+    @bottles = User.find_by(id: current_user).appointments.sum(:bottle)
   end
 end
