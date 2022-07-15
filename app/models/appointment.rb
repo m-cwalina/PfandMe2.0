5c884d9cee67cc6e61c82831abb8c9c95cd3931b
Matthew Cwalina <matthewcwalina@gmail.com>
2022-07-15 18:24:58 +0200

    Added validations to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :picker
+  validates :date, :time, :address, :bottle, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   acts_as_mappable :default_units => :miles,
