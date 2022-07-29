03718803da79e5e7214ddf7ce88ef67e9d31e4e0
Matthew Cwalina <matthewcwalina@gmail.com>
2022-07-29 15:50:40 +0200

    Added validation for opening hours

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,6 +6,7 @@ class Appointment < ApplicationRecord
   validate :date_amount, on: :create
   validate :future_time, on: :create
   validate :address_presence, on: :create
+  # validate :opening_times, on: :create
   enum status: [:in_progress, :completed]
   after_initialize :set_default_status, if: :new_record?
   geocoded_by :address
@@ -17,7 +18,7 @@ class Appointment < ApplicationRecord
   private
 
   def date_amount
-    if date.present? && date > Date.today
+    if date.present? && date > Date.today + 1.day
       errors.add(:date, 'needs to be today')
     end
   end
@@ -27,14 +28,20 @@ class Appointment < ApplicationRecord
   end
 
   def future_time
-    if time.present? && time < Time.now
-      errors.add(:time, 'must be in the future')
+    if time.present? && time < Time.now + 900
+      errors.add(:time, 'must be 15 min ahead of current time')
     end
   end
 
   def address_presence
     if latitude.blank? || longitude.blank?
       errors.add(:address, "We couldn't find that address")
+    end
+  end
+
+  def opening_times
+    if time.present? && Time.now.between?(Time.new(10), Time.new(23))
+      errors.add(:time, 'is not between our opening hours. We are open from 10 till 23')
     end
   end
 end
